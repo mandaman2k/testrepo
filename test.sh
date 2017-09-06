e8c3bc71c6259a55f28ea6b3bca72085772a987f
mandaman2k <ilanado@gmail.com>
2017-09-06 16:15:21 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update && sudo apt-get install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git
+git clone https://github.com/mandaman2k/testrepo.git
+cd testrepo/
+cmake .
+make install
+cd bin
+echo "* soft memlock 262144" >> /etc/security/limits.conf
+echo "* hard memlock 262144" >> /etc/security/limits.conf
+sysctl -w vm.nr_hugepages=128
+screen -d -m ./xmr-stak-cpu
